4231227a4ceba3a87fcf77a687380399dd161ead
KPtheRed <101702847+KPtheRed@users.noreply.github.com>
2023-05-23 23:14:14 +0800

    Add files via upload

---
+++ QuizModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Quizproject
+{
+    public class QuizModel
+    {
+        public String question { get; set; }
+        public String questionName { get; set; }
+
+        public List<Answer> answers { get; set; }
+    }
+
+    public class Answer
+    {
+        public string answer { get; set; }
+        public bool isRightAns { get; set; }
+    }
+
+    public class QuestionVIewModel
+    { 
+        public QuizModel qone { get; set; }
+        public QuizModel qtwo { get; set; }
+        public QuizModel qthree { get; set; }
+        public QuizModel qfour { get; set; }
+
+    }
+}
